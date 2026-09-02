f280e2c09191e8ce19c542d41b892e4f7eb3c5a1
bajoicheg <bbajoi@gmail.com>
2026-09-02 20:41:49 +0300

    Bump Windows metadata to 0.9.0

--- build.rs
+++ build.rs
@@ -7,8 +7,8 @@ fn main() {
         resource.set("ProductName", "G-switcher");
         resource.set("CompanyName", "V. Vasilev");
         resource.set("LegalCopyright", "Copyright (c) V. Vasilev 2026");
-        resource.set("FileVersion", "0.8.2.0");
-        resource.set("ProductVersion", "0.8.2");
+        resource.set("FileVersion", "0.9.0.0");
+        resource.set("ProductVersion", "0.9.0");
         resource
             .compile()
             .expect("failed to compile Windows resources");
