cbe457d80fa66911d402570230c8bccb15e678d0
bajoicheg <bbajoi@gmail.com>
2026-09-02 12:13:19 +0300

    Embed G-switcher Windows resources

---
+++ build.rs
@@ -0,0 +1,14 @@
+fn main() {
+    #[cfg(windows)]
+    {
+        let mut resource = winres::WindowsResource::new();
+        resource.set_icon("assets/g-switcher.ico");
+        resource.set("FileDescription", "G-switcher");
+        resource.set("ProductName", "G-switcher");
+        resource.set("CompanyName", "V. Vasilev");
+        resource.set("LegalCopyright", "Copyright (c) V. Vasilev 2026");
+        resource.set("FileVersion", "0.6.0.0");
+        resource.set("ProductVersion", "0.6.0");
+        resource.compile().expect("failed to compile Windows resources");
+    }
+}
