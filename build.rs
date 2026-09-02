2fed8462bf75a28cacb581c3a8eabe9c51fe9f27
bajoicheg <bbajoi@gmail.com>
2026-09-02 23:23:50 +0300

    Update Windows metadata for 1.0.0

--- build.rs
+++ build.rs
@@ -7,8 +7,8 @@ fn main() {
         resource.set("ProductName", "G-switcher");
         resource.set("CompanyName", "V. Vasilev");
         resource.set("LegalCopyright", "Copyright (c) V. Vasilev 2026");
-        resource.set("FileVersion", "0.9.0.0");
-        resource.set("ProductVersion", "0.9.0");
+        resource.set("FileVersion", "1.0.0.0");
+        resource.set("ProductVersion", "1.0.0");
         resource
             .compile()
             .expect("failed to compile Windows resources");
