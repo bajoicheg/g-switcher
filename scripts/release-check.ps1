f9d6715cd0ae90c6d374378da15b15ae4c5ef4e2
bajoicheg <bbajoi@gmail.com>
2026-09-03 10:45:15 +0300

    Update release checks for 1.0.1

--- scripts/release-check.ps1
+++ scripts/release-check.ps1
@@ -45,10 +45,10 @@ if ($version.FileDescription -ne 'G-switcher') {
 if ($version.ProductName -ne 'G-switcher') {
     throw "Unexpected ProductName: $($version.ProductName)"
 }
-if ($version.FileVersion -ne '1.0.0.0') {
+if ($version.FileVersion -ne '1.0.1.0') {
     throw "Unexpected FileVersion: $($version.FileVersion)"
 }
-if ($version.ProductVersion -ne '1.0.0') {
+if ($version.ProductVersion -ne '1.0.1') {
     throw "Unexpected ProductVersion: $($version.ProductVersion)"
 }
 
